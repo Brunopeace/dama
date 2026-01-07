87cc336f348c4a43711086a3095aec631b565985
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2026-01-06 22:57:07 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'damas-pro-v3';
 const ASSETS = [
-  './',
-  './index.html',
-  './css/styles.css',
-  './js/script.js',
-  './img/icon-192.png',
-  './img/icon-512.png',
-  './movimento.mp3',
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/script.js',
+  '/img/icon-192.png',
+  '/img/icon-512.png',
+  '/movimento.mp3',
 ];
 
 // Instalação: Salva os arquivos no cache
